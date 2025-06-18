86454434909a52fe8011bef6ec89d3a1024ed242
Kaustubh Bhoir <kaustubhbhoir173@gmail.com>
2025-06-18 13:39:48 +0530

    change in dadhboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import authMiddleware from '../middleware/authMiddleware.js'
-import { getSummary } from '../controllers/dashboardcontroller.js'
+import { getSummary } from '../controllers/dashboardController.js'
 
 const router = express.Router()
 
